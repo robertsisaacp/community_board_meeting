cec0f75516bfabc4967240f35beb7348426078ef
sarahJune1 <sarahjuneoriginal@gmail.com>
2020-08-11 20:20:02 -0400

    update output and clean filler words

--- src/create_summary.py
+++ src/create_summary.py
@@ -3,12 +3,10 @@ from gensim.summarization import summarize
 from punctuator import Punctuator
 import collections
 import os
-
-# import default pre-trained model from punctuator
-p = Punctuator('Demo-Europarl-EN.pcl')
+from pathlib import Path
 
 
-def get_transcript(video_id=None):
+def get_transcript(video_id):
     """
     With id from youtube, obtain transcript and join each text phrase into one chunk
     :param video_id: id of youtube video
@@ -29,13 +27,27 @@ def get_transcript(video_id=None):
     return " ".join("{}".format(line) for line in transcript_text)
 
 
-def add_punctuation(text_input=None):
+def clean_transcript(text_input):
+    """
+    Removes filler words from transcript
+    :return:
+    """
+    filler_words = ['um', 'uh', '[music]']
+    query_words = text_input.split()
+
+    result_words = [word for word in query_words if word.lower() not in filler_words]
+    return ' '.join(result_words)
+
+
+def add_punctuation(text_input):
     """
     Using Punctuator2 from https://github.com/ottokart/punctuator2
     Breakdown the transcript chunk into sentences with grammar.
     :param text_input: transcript input
     :return: transcript input with grammar added
     """
+    # import default pre-trained model from punctuator
+    p = Punctuator('Demo-Europarl-EN.pcl')
 
     text_input = None if text_input is None else text_input
 
@@ -58,27 +70,37 @@ def summarize_text(text_input=None, ratio_input=None):
 
 def output_transcript():
     """
-    Save transcript to text file
+    Save transcript and summary file to text files
     :return:
     """
-    with open(f'community_board_transcript_{transcript_id}.txt', 'w') as f:
+
+    transcript_folder_path = Path("transcripts/")
+    new_transcript_dir = os.path.join(transcript_folder_path, transcript_id)
+    os.makedirs(new_transcript_dir, exist_ok=True)
+
+    # output full transcript
+    with open(f'{new_transcript_dir}//full_transcript_{transcript_id}.txt', 'w') as f:
+        f.write(summary_input)
+
+    # output summary transcript
+    with open(f'{new_transcript_dir}//summary_{ratio_of_transcript*100}%_transcript_{transcript_id}.txt', 'w') as f:
         f.write(summary_output)
 
 
 if __name__ == "__main__":
-    OUTPUT_DIR = "community_board_meeting/transcripts/"
-    print(os.getcwd())
-
     transcript = collections.defaultdict(list)
     transcript_id = input('Enter id from Youtube link:')
     print(f'Obtaining transcript for {transcript_id}')
     meeting = get_transcript(transcript_id)
     print('Transcript obtained!')
+    print('Removing uh any um filler words')
+    meeting = clean_transcript(meeting)
     print('Splitting into Sentences')
     summary_input = add_punctuation(meeting)
     print('Sentences ready for summarization.')
     print('Saving file output')
-    summary_output = summarize_text(summary_input, .10)
+    ratio_of_transcript = .40
+    summary_output = summarize_text(summary_input, ratio_of_transcript)
     print('Your Community Board transcript is ready!')
     output_transcript()
 
